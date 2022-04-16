811cee28d801d078ee9975f2fed3288d22a90c87
Selin Denise Altiparmak <selin@Selins-MBP.fios-router.home>
2022-04-16 10:58:14 -0400

    reading from terminal using arg -r,-rN  and -rN,M plus tried to seperate file_queue and dir_queue

--- src/utils.h
+++ src/utils.h
@@ -11,5 +11,6 @@
 void print_buffer(char *word_buffer, int length);
 ssize_t safe_write(int fd, const void *__buf, size_t __nbyte);
 int check_file_or_directory(struct stat *file_in_dir_pointer);
+int get_run_mode(char *arg,int *N, int *M); 
 
 #endif

af3c4a6458957402c947e4fe67fa3384db4f21ef
Selin D. Altiparmak <sda81@ilab1.cs.rutgers.edu>
2022-04-18 11:38:13 -0400

    __VA_ARGS__ solved()

--- src/logger.h
+++ src/logger.h
@@ -1,12 +1,12 @@
 #pragma once
-#define DEBUG 0
-#define debug_print(fmt, __VA_ARGS__...)                                                    \
+#define DEBUG 1
+#define debug_print(fmt, ...)                                                    \
     do                                                                                      \
     {                                                                                       \
         if (DEBUG)                                                                          \
             fprintf(stdout, "%s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, __VA_ARGS__); \
     } while (0)
-#define error_print(fmt, __VA_ARGS__...)                                                \
+#define error_print(fmt, ...)                                                \
     do                                                                                  \
     {                                                                                   \
         fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, __VA_ARGS__); \
